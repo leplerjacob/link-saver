80a8e1f9e4732194540c942d7633501aebfe31fd
Jacob Lepler <lepler.jacob@gmail.com>
2021-10-20 22:18:34 -0400

    add button switch from add to submit

--- popup.js
+++ popup.js
@@ -2,6 +2,8 @@ import UI from "./UI.js";
 
 document.getElementById("add").addEventListener("click", (event) => {
   event.preventDefault();
+  event.target.classList.add("submit")
+  event.target.innerText = "Submit"
   // Listen for user to click "Add" button
   UI.addInputBoxes();
 });
